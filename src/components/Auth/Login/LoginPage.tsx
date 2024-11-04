517e425223997f56408ad8a24b1a2b3eac7b3ff0
wodudttjs <115385658+wodudttjs@users.noreply.github.com>
2024-11-04 13:19:58 +0900

    로그인페이지 사이즈 이슈 수정

--- src/components/Auth/Login/LoginPage.tsx
+++ src/components/Auth/Login/LoginPage.tsx
@@ -69,7 +69,7 @@ const LoginPage: React.FC = () => {
           placeholder="      아이디 (학번/사번)"
         />
 
-
+      
           <label htmlFor="password" className={styles['visually-hidden']}>
             비밀번호
           </label>
