6a34e09497159418a7fa9a3bc59d4a860cbe7408
wodudttjs <115385658+wodudttjs@users.noreply.github.com>
2024-11-12 16:01:23 +0900

    비밀번호 버튼 구헌 0

--- src/components/Members/UserProfile/FormCard.tsx
+++ src/components/Members/UserProfile/FormCard.tsx
@@ -7,29 +7,36 @@ interface FormCardProps {
     id: string;
     placeholder: string;
     type?: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   }>;
-  submitButtonSrc: string;
+  buttonText: string;
+  onSubmit: (event: React.FormEvent) => void;
 }
 
-export const FormCard: React.FC<FormCardProps> = ({ title, inputs, submitButtonSrc }) => {
+const FormCard: React.FC<FormCardProps> = ({ title, inputs, buttonText, onSubmit }) => {
   return (
-    <form className={styles.formCard}>
+    <form className={styles.formCard} onSubmit={onSubmit}>
       <h2 className={styles.formTitle}>{title}</h2>
-      {inputs.map(({ id, placeholder, type = 'text' }) => (
+      {inputs.map(({ id, placeholder, value, type = 'text', onChange }) => (
         <React.Fragment key={id}>
           <label htmlFor={id} className={styles['visually-hidden']}>{placeholder}</label>
           <input
             type={type}
             id={id}
+            value={value}
             className={styles.formInput}
             placeholder={placeholder}
             aria-label={placeholder}
+            onChange={onChange}  
           />
         </React.Fragment>
       ))}
       <button type="submit" className={styles.submitButton}>
-        <img src={submitButtonSrc} alt="Submit" className={styles.submitButton} />
+        {buttonText}
       </button>
     </form>
   );
-}
+};
+
+export default FormCard;
