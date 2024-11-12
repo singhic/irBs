8d0fe0eadbeb74220910b5cb21a10ac53733b6a9
SeoSangHyeok <ssb50800@gmail.com>
2024-11-13 01:08:27 +0900

    feat: update BusSchedule and ScheduleCard components for improved functionality and styling

--- src/components/Main/BusSchedule/ScheduleCard.tsx
+++ src/components/Main/BusSchedule/ScheduleCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { BusRoute } from './types.ts';
 import styles from './BusSchedule.module.css';
 
@@ -11,16 +12,23 @@ interface ScheduleCardProps {
 }
 
 export const ScheduleCard: React.FC<ScheduleCardProps> = ({ route, schedule }) => {
+  const navigate = useNavigate();
+
   // 배열을 "/ "로 구분된 문자열로 변환
   const toSchoolTimes = schedule.toSchool.join(' / ');
   const fromSchoolTimes = schedule.fromSchool.join(' / ');
 
+  // 카드 클릭 시 이동하는 함수
+  const handleClick = () => {
+    navigate(`/SeatMap?lineCode=${route.value}`);
+  };
+
   return (
-    <div className={styles.scheduleCard}>
+    <div className={styles.scheduleCard} onClick={handleClick}>
       <h3 className={styles.locationName}>{route.location}</h3>
       <div className={styles.departureTime}>
-        <p>등교: {toSchoolTimes || '배차 없음'}</p> {/* 등교 시간 */}
-        <p>하교: {fromSchoolTimes || '배차 없음'}</p> {/* 하교 시간 */}
+        <p className={styles.timeText}>등교: {toSchoolTimes || '배차 없음'}</p>
+        <p className={styles.timeText}>하교: {fromSchoolTimes || '배차 없음'}</p>
       </div>
     </div>
   );
