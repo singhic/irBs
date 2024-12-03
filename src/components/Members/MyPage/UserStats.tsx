d7eee21428e5faf9ad18f03fcd974d1eab333cf9
SeoSangHyeok <ssb50800@gmail.com>
2024-12-03 10:52:19 +0900

    fix. unable service notice

--- src/components/Members/MyPage/UserStats.tsx
+++ src/components/Members/MyPage/UserStats.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import styles from './MyPage.module.css';
-import { UserStatsProps } from './types';
+import React from "react";
+import styles from "./MyPage.module.css";
+import { UserStatsProps } from "./types";
 
-export const UserStats: React.FC<UserStatsProps> = ({ penaltyCount, mannerScore }) => (
+export const UserStats: React.FC<UserStatsProps> = ({
+  penaltyCount,
+  mannerScore,
+}) => (
   <div className={styles.statsContainer}>
     <div className={styles.statCard}>
       <div className={styles.statTitle}>
         <span>패널티 누적</span>
-        <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/6e320303532684b9d1bb5b1184861621757f154f2bef4f1038b401b69c63e230?placeholderIfAbsent=true" alt="" className={styles.icon} />
+        <img
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/6e320303532684b9d1bb5b1184861621757f154f2bef4f1038b401b69c63e230?placeholderIfAbsent=true"
+          alt=""
+          className={styles.icon}
+        />
       </div>
-      <div className={styles.statValue}>{penaltyCount}회</div>
+      <div className={styles.statValue}>{penaltyCount}</div>
     </div>
     <div className={styles.statCard}>
       <div className={styles.statTitle}>비매너 점수</div>
