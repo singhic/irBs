d3ac9ef4b06375f441eb951b1b80cbd9d578f96c
SeoSangHyeok <ssb50800@gmail.com>
2025-01-21 20:56:10 +0900

    fix. resize list page

--- src/components/List/InquiryList/InquiryList.tsx
+++ src/components/List/InquiryList/InquiryList.tsx
@@ -8,19 +8,19 @@ const inquiryData: InquiryItem[] = [
     date: "2024. 10. 22",
     type: "불편사항",
     iconSrc:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/a82e4647fab3042a2ba57e555ed603539964fa0db4599cd8b8e459485263cf38?placeholderIfAbsent=true&apiKey=c37b27135006443aab5f3858d3155b30",
+      "img/icon/arrow-right.png",
   },
   {
     date: "2024. 10. 12",
     type: "패널티",
     iconSrc:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/12ebfeda3ad9c2aa6919509b3dcb8c1377d170f8578674980383bac8f487ba99?placeholderIfAbsent=true&apiKey=c37b27135006443aab5f3858d3155b30",
+      "img/icon/arrow-right.png",
   },
   {
     date: "2024. 10. 12",
     type: "미탑승",
     iconSrc:
-      "https://cdn.builder.io/api/v1/image/assets/TEMP/31a02d5f5e2e6d181b4d194201b00af0ea385495650535869564dee54ddc7dea?placeholderIfAbsent=true&apiKey=c37b27135006443aab5f3858d3155b30",
+      "img/icon/arrow-right.png",
   },
 ];
 
