974080a678e2c06eff801147e2ceaa756509e411
SeoSangHyeok <89758324+singhic@users.noreply.github.com>
2025-02-06 16:28:19 +0900

    Update MainPage.tsx

--- src/components/Main/MainPage/MainPage.tsx
+++ src/components/Main/MainPage/MainPage.tsx
@@ -341,7 +341,7 @@ export const MainPage: React.FC = () => {
 
           <a
             className={styles.noUnderline}
-            href="\NoticePDF\통학버스 이용안내.pdf"
+            href="\NoticePDF\Shuttle_Bus_Usage_Guide.pdf"
             target="_blank" // 새 탭에서 열기
             rel="noopener noreferrer" // 보안 설정
           >
